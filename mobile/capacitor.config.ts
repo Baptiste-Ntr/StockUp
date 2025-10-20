29d8bdaa67990841adec13f39edbcb6a2e00d2b5
Baptise_Ntr <nautre.baptiste@hotmail.com>
2025-10-20 23:57:33 +0200

    init capacitor

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lynupstock.app',
+  appName: 'Lynup Stock',
+  webDir: 'dist'
+};
+
+export default config;
